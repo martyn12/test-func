065ba86f57ca299db7f64e9139fa24ebea8e9821
martyn12 <87023187+martyn12@users.noreply.github.com>
2024-03-05 09:55:51 +0400

    add gitignore and README

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+include 'vendor/autoload.php';
+
+use Src\Converter;
+
+echo Converter::revertCharacters("Привет! Давно не виделись.");
--- src/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include 'vendor/autoload.php';
-
-use Src\Converter;
-
-//echo "Привет! Давно не виделись.";
-echo Converter::revertCharacters(12312.3123);
